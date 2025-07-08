f5f24fb960f64961a06a9f2659fa2439118e6149
ChrisMathieu20 <mathieurchris@gmail.com>
2025-07-08 07:42:53 +0300

    Week01 - Exercise4 (Milestone)

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> lists = new List<int>();
+
+        int series = 1;
+
+        while (series != 0)
+        {
+            if (series > 0)
+            {
+                Console.Write("Enter a series of number: ");
+                string seriesNumber = Console.ReadLine();
+                series = int.Parse(seriesNumber);
+
+                lists.Add(series);
+            }
+        }
+
+        int sum = 0;
+        
+        foreach (int list in lists)
+        {
+            int add = list;
+            sum = sum + add;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
