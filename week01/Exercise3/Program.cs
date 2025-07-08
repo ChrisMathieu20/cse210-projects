709139d01c623f213f5d63918badbe0f41d0900b
ChrisMathieu20 <mathieurchris@gmail.com>
2025-07-08 07:40:52 +0300

    Week01 - Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool continu = true;
+
+        while (continu == true)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,10);
+
+            int guess = 0;
+            int count = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                count++;
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Guesses: {count}");
+
+            Console.Write("Do you want to play again? yes or no -- ");
+            string question = Console.ReadLine();
+            
+            if (question == "yes")
+            {
+                continu = true;
+            }
+            else
+            {
+                continu = false;
+            }
+        }
     }
 }
