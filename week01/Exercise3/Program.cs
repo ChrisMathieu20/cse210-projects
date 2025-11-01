a61fbdc4dd014ea252f44af9f808515a3680545a
ChrisMathieu20 <cramasinjanahary@byupathway.edu>
2025-11-01 21:07:21 +0300

    Guess My Number Game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        bool isAgain = true;
+        while (isAgain)
+        {
+            bool isGuessed = true;
+            int i = 0;
+            while (isGuessed)
+            {
+                // Ask user for a guess
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                int guess = int.Parse(userGuess);
+
+                // Determine if the user needs to guess higher or lower or they guessed it
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    isGuessed = true;
+                    i++;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    isGuessed = true;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    isGuessed = false;
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"You guesses {i} times.");
+
+            Console.Write("Do you want to play again? ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes")
+            {
+                isAgain = true;
+            }
+            else
+            {
+                isAgain = false;
+            }
+        }
     }
 }
