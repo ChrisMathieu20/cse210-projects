fcc76c6613cf8b51cb2cc5c4f490d16e046b7e32
ChrisMathieu20 <cramasinjanahary@byupathway.edu>
2025-12-03 23:14:22 +0300

    Polymorphism - Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square sq = new Square("blue",4);
+        Rectangle re = new Rectangle("purple",6,2);
+        Circle ci = new Circle("yellow",5);
+
+        shapes.Add(sq);
+        shapes.Add(re);
+        shapes.Add(ci);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Shape > Color: {color} - Area: {area}");
+        }
     }
 }
